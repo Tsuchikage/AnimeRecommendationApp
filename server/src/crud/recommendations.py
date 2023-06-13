c581117efad6642f026d2d099dd90b52c43202ee
nevsky116 <nevsky116@gmail.com>
2023-06-13 19:32:16 +0300

    Add content-based

--- server/src/crud/recommendations.py
+++ server/src/crud/recommendations.py
@@ -5,16 +5,24 @@ from pymongo.database import Database
 from bson import ObjectId
 from datetime import datetime
 
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 settings = get_settings()
 
-# Функции, которая возвращает результаты по каждому поисковому аниме отдельно, 
-# а также объединенный набор данных с 10 лучшими рекомендациями по всем поисковым аниме на основе наименьшего расстояния:
-async def get_recommendations(req: Request, search_words, n_recommendations=10) -> list:
-    anime_data = list(await req.app.db.animelist.find().to_list(length=None))  # Загрузка данных аниме из базы данных
+async def load_anime_data(db: Database):
+    anime_data = list(await db.animelist.find().to_list(length=None))  # Загрузка данных аниме из базы данных
     
     for document in anime_data:
         document['id'] = str(document['_id'])
-        document.pop("_id")
+        document.pop("_id")    
+
+    return anime_data
+
+# Функции, которая возвращает результаты по каждому поисковому аниме отдельно, 
+# а также объединенный набор данных с 10 лучшими рекомендациями по всем поисковым аниме на основе наименьшего расстояния:
+async def get_item_based_recommendations(req: Request, search_words, n_recommendations=10) -> list:
+    anime_data = await load_anime_data(req.app.db)
 
     recommendations = []
     knn = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=20, n_jobs=-1)
@@ -45,6 +53,44 @@ async def get_recommendations(req: Request, search_words, n_recommendations=10) 
 
     return recommendations
 
+async def get_content_based_recommendations(req: Request, search_words, n_recommendations=10):
+    anime_data = await load_anime_data(req.app.db)
+
+    # Создание матрицы признаков на основе синопсисов (content-based)
+    content_matrix = pd.DataFrame(anime_data)  # Создаем DataFrame из данных аниме
+    content_matrix['synopsis'] = content_matrix['synopsis'].fillna('')  # Заполняем пропущенные значения в столбце "synopsis" пустой строкой
+
+    tfidf = TfidfVectorizer(stop_words='english')  # Создаем объект TfidfVectorizer для создания матрицы TF-IDF
+    tfidf_matrix = tfidf.fit_transform(content_matrix['synopsis'].values.astype('U'))  # Преобразуем синопсисы в TF-IDF матрицу признаков
+
+    knn = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=n_recommendations+1, n_jobs=-1)  # Инициализируем модель NearestNeighbors для поиска ближайших соседей
+    knn.fit(tfidf_matrix)  # Обучаем модель на матрице признаков
+
+    recommendations = []
+
+    for word in search_words:
+        anime_search = content_matrix[content_matrix['title'].str.contains(word, case=False)]  # Ищем аниме, в названии которого есть заданное слово (без учета регистра)
+
+        if anime_search.empty:
+            continue
+
+        anime_ids = anime_search['anime_id'].values
+        anime_recommendations = []
+
+        for anime_id in anime_ids:
+            anime_index = content_matrix[content_matrix['anime_id'] == anime_id].index[0]
+            distances, indices = knn.kneighbors(tfidf_matrix[anime_index], n_neighbors=n_recommendations + 1)
+            indices_list = indices.squeeze()[1:].tolist()  # Исключаем первый элемент, который является самим аниме
+            anime_recommendations.extend(indices_list)
+
+        anime_recommendations = list(set(anime_recommendations))[:n_recommendations]  # Извлекаем n уникальных рекомендаций
+
+        for anime_index in anime_recommendations:
+            anime_info = content_matrix.loc[anime_index].to_dict()
+            if anime_info['anime_id'] not in anime_ids:
+                recommendations.append(anime_info)
+    print(recommendations)
+    return recommendations
 
 async def create_recommendation(db: Database, user_id: str, recommendations: list, search_words: list):
     data = []
--- server/src/routers/recommendations.py
+++ server/src/routers/recommendations.py
@@ -1,17 +1,28 @@
 from fastapi import APIRouter, Depends, Request, HTTPException, status
 from dependencies import get_current_user
 from models import User, RecommendationPayload
-from crud.recommendations import get_recommendations, create_recommendation, find_recommendation
+from crud.recommendations import get_item_based_recommendations, get_content_based_recommendations, create_recommendation, find_recommendation
 
 
 router = APIRouter(tags=["recommendations"], prefix="/recommendations")
 
 
-@router.post("/")
-async def generate_recommendations(payload: RecommendationPayload,
+@router.post("/item-based")
+async def generate_item_based_recommendations(payload: RecommendationPayload,
                                    request: Request,
                                    user: User = Depends(get_current_user)):
-    recommendations = await get_recommendations(request, payload.search_words, payload.count)
+    recommendations = await get_item_based_recommendations(request, payload.search_words, payload.count)
+
+    recommendaiton_id = await create_recommendation(request.app.db, user['id'], recommendations, payload.search_words)
+
+    return await find_recommendation(request.app.db, recommendaiton_id)
+
+
+@router.post("/content-based")
+async def generate_content_based_recommendations(payload: RecommendationPayload,
+                                   request: Request,
+                                   user: User = Depends(get_current_user)):
+    recommendations = await get_content_based_recommendations(request, payload.search_words, payload.count)
 
     recommendaiton_id = await create_recommendation(request.app.db, user['id'], recommendations, payload.search_words)
 
